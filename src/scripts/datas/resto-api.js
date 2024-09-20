45b07d98f846337619ec524f3734022bca377a55
Tods <algy25ng@gmail.com>
2024-09-20 09:26:06 +0800

    endpoint

--- src/scripts/datas/resto-api.js
+++ src/scripts/datas/resto-api.js
@@ -1,7 +1,9 @@
+const endpoint = "./data/DATA.json";
+
 class RestoApi {
   static async getResto() {
     try {
-      const response = await fetch("./data/DATA.json");
+      const response = await fetch(`${endpoint}`);
       const responseJson = await response.json();
       const data = responseJson.restaurants;
 
