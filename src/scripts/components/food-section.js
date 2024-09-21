2079ab785637e632c2950da26a0ccd615a28f184
Isshoo <algy25ng@gmail.com>
2024-09-21 19:13:57 +0800

    food-section.js and style

---
+++ src/scripts/components/food-section.js
@@ -0,0 +1,47 @@
+class FoodSection extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  _emptyContent() {
+    this.innerHTML = "";
+  }
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this._emptyContent();
+    this.innerHTML += `
+    <div class="food-con">
+          <div class="food-title">
+          <h2 class="section-title"><span>Best Seller Menu</span></h2>
+        </div>
+        <div class="food-content">
+          <article class="food-item">
+            <img src="./images/food/lalapan.jpg" alt="Lalapan" />
+            <h3>Lalapan</h3>
+          </article>
+          <article class="food-item">
+            <img src="./images/food/martabak.jpg" alt="Martabak" />
+            <h3>Martabak</h3>
+          </article>
+          <article class="food-item">
+            <img src="./images/food/mie-ceplok.jpg" alt="Mie Ceplok" />
+            <h3>Mie Ceplok</h3>
+          </article>
+          <article class="food-item">
+            <img src="./images/food/nasi-campur.jpg" alt="Nasi Campur" />
+            <h3>Nasi Campur</h3>
+          </article>
+          <article class="food-item">
+            <img src="./images/food/sate.jpg" alt="Sate" />
+            <h3>Sate</h3>
+          </article>
+        </div>
+        </div>
+        `;
+  }
+}
+
+customElements.define("food-section", FoodSection);
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,7 @@ import "./components/footer-bar.js";
 import "./components/header-bar.js";
 import "./components/hero-section.js";
 import "./components/explore-section.js";
+import "./components/food-section.js";
 import "./components/resto-item.js";
 import { home } from "./view/home.js";
 
