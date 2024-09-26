9c68df276c7aa19a327019f19a85614404bedc49
Tods <algy25ng@gmail.com>
2024-09-26 20:20:16 +0800

    cache di service worker

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,18 +1,32 @@
-self.addEventListener('install', () => {
-  console.log('Installing Service Worker ...');
+import 'regenerator-runtime';
+import CacheHelper from './utility/cache-helper';
 
-  // TODO: Caching App Shell Resource
+// Daftar asset yang akan dicaching
+const assetsToCache = [
+  './',
+  './icon/icons/restaurant-72.png',
+  './icon/icons/restaurant-96.png',
+  './icon/icons/restaurant-128.png',
+  './icon/icons/restaurant-144.png',
+  './icon/icons/restaurant-152.png',
+  './icon/icons/restaurant-192.png',
+  './icon/icons/restaurant-384.png',
+  './icon/icons/restaurant-512.png',
+  './index.html',
+  './icon/favicon.png',
+  './app.bundle.js',
+  './app.webmanifest',
+  './sw.bundle.js',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(CacheHelper.cachingAppShell([...assetsToCache]));
 });
 
-self.addEventListener('activate', () => {
-  console.log('Activating Service Worker ...');
-
-  // TODO: Delete old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(CacheHelper.deleteOldCache());
 });
 
 self.addEventListener('fetch', (event) => {
-  console.log(event.request);
-
-  event.respondWith(fetch(event.request));
-  // TODO: Add/get fetch request to/from caches
+  event.respondWith(CacheHelper.revalidateCache(event.request));
 });
