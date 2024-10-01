5c5d154451f4dfde76a1ddef9c3d915b2dc2190e
Isshoo <algy25ng@gmail.com>
2024-10-02 03:03:53 +0800

    menampilkan data menus

--- src/scripts/components/resto-menus.js
+++ src/scripts/components/resto-menus.js
@@ -22,18 +22,42 @@ class RestoMenus extends HTMLElement {
     this.innerHTML = '';
   }
 
+  displayItem() {
+    const foodsCon = this.querySelector('#foodsList');
+    const foodsList = this.resto.foods.map((food) => {
+      const foodList = document.createElement('li');
+      foodList.textContent = food;
+      return foodList;
+    });
+    foodsCon.innerHTML = '';
+    foodsCon.append(...foodsList);
+
+    const drinksCon = this.querySelector('#drinksList');
+    const drinksList = this.resto.drinks.map((drink) => {
+      const drinkList = document.createElement('li');
+      drinkList.textContent = drink;
+      return drinkList;
+    });
+
+    drinksCon.innerHTML = '';
+    drinksCon.append(...drinksList);
+  }
+
   render() {
     this.emptyContent();
     this.innerHTML += `
-         <article class="resto-card">
-                <p>
-                ${this.resto.foods}
-                </p>
-                <p>
-                ${this.resto.drinks}
-                </p>
+         <article class="menus-container">
+                <div>
+                <h3>Foods</h3>
+                 <ul id="foodsList"></ul>
+                </div>
+                <div>
+                <h3>Drinks</h3>
+                 <ul id="drinksList"></ul>
+                </div>
             </article>
         `;
+    this.displayItem();
   }
 }
 
