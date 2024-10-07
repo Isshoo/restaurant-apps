9119d67423f96c3ede1da154016d1a84d48ce3e4
Tods <algy25ng@gmail.com>
2024-10-08 03:00:25 +0800

    testing skenario menyukai film (90%)

--- src/scripts/view/templates/template-creator.js
+++ src/scripts/view/templates/template-creator.js
@@ -1,10 +1,10 @@
 const createLikeButtonTemplate = () => `
-  <button aria-label="like this movie" id="likeButton" class="like">
+  <button aria-label="like this resto" id="likeButton" class="like">
     <i class="fa-solid fa-plus"></i> <span>Add to Favorite</span>
   </button>
 `;
 const createLikedButtonTemplate = () => `
-  <button aria-label="unlike this movie" id="likedButton" class="like">
+  <button aria-label="unlike this resto" id="likedButton" class="like">
     <i class="fa-solid fa-check"></i> <span>Favorited</span>
   </button>
 `;
---
+++ tests/likeResto.test.js
@@ -0,0 +1,81 @@
+import LikeButtonInitiator from '../src/scripts/utility/like-button-initiator';
+import FavoriteRestoIdb from '../src/scripts/datas/favorite-resto-idb';
+
+describe('Add A Resto to Favorited', () => {
+  const addLikeButtonContainer = () => {
+    document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+  };
+
+  beforeEach(() => {
+    addLikeButtonContainer();
+  });
+
+  it('should show the favorite button when the resto has not been favorited before', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      resto: {
+        id: 1,
+      },
+    });
+
+    expect(document.querySelector('[aria-label="like this resto"]')).toBeTruthy();
+  });
+
+  it('should not show the unfavorite button when the resto has not been favorited before', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      resto: {
+        id: 1,
+      },
+    });
+    expect(document.querySelector('[aria-label="unlike this resto"]')).toBeFalsy();
+  });
+
+  it('should be able to like the resto', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      resto: {
+        id: 1,
+      },
+    });
+
+    document.querySelector('#likeButton').dispatchEvent(new Event('click'));
+
+    // Memastikan resto berhasil disukai
+    const resto = await FavoriteRestoIdb.getResto(1);
+    expect(resto).toEqual({ id: 1 });
+
+    await FavoriteRestoIdb.deleteResto(1);
+  });
+
+  it('should not add a resto again when its already liked', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      resto: {
+        id: 1,
+      },
+    });
+
+    // Tambahkan resto dengan ID 1 ke daftar film yang disukai
+    await FavoriteRestoIdb.putResto({ id: 1 });
+
+    // Simulasikan pengguna menekan tombol favorite
+    document.querySelector('#likeButton').dispatchEvent(new Event('click'));
+
+    // Tidak ada resto yang ganda
+    expect(await FavoriteRestoIdb.getAllResto()).toEqual([{ id: 1 }]);
+    await FavoriteRestoIdb.deleteResto(1);
+  });
+
+  // Menggunakan metode xit, bukan it, untuk menonaktifkan sebuah test case
+  xit('should not add a resto when it has no id', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      resto: {},
+    });
+
+    document.querySelector('#likeButton').dispatchEvent(new Event('click'));
+
+    expect(await FavoriteRestoIdb.getAllResto()).toEqual([]);
+  });
+});
