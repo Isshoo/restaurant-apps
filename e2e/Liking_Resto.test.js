30ac1737f959985a533cd9ffe4dfc0ea4692307e
Tods <algy25ng@gmail.com>
2024-10-16 01:14:06 +0800

    tes e2e awal

--- e2e/Liking_Resto.test.js
+++ e2e/Liking_Resto.test.js
@@ -1,5 +1,11 @@
+/* eslint-disable no-undef */
 Feature('Liking Resto');
 
-Scenario('test something',  ({ I }) => {
+Before(({ I }) => {
+  I.amOnPage('/#/favorite');
+});
 
+Scenario('showing empty liked resto', ({ I }) => {
+  I.seeElement('#searchForm');
+  I.see('', '#resto-list');
 });
