99bea286778b49cbf8aadff9c952584aac5db7d2
Tods <algy25ng@gmail.com>
2024-11-20 05:58:39 +0800

    no image webp

--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const ImageminWebpWebpackPlugin = require('imagemin-webp-webpack-plugin');
+// const ImageminWebpWebpackPlugin = require('imagemin-webp-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
@@ -11,6 +11,7 @@ const CompressionPlugin = require('compression-webpack-plugin');
 const BrotliPlugin = require('brotli-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CriticalCssPlugin = require('critical-css-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 require('dotenv').config({
   path: path.resolve('.env'),
@@ -111,16 +112,10 @@ module.exports = {
 
     new CriticalCssPlugin(),
 
-    new ImageminWebpWebpackPlugin({
-      config: [
-        {
-          test: /\.(jpe?g|png)/,
-          options: {
-            quality: 50,
-          },
-        },
-      ],
-      overrideExtension: true,
+    new ImageMinimizerPlugin({
+      minimizerOptions: {
+        plugins: ['imagemin-webp'],
+      },
     }),
 
     new CompressionPlugin({
